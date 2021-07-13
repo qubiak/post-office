4900952f12de68800fec7d8d1e0cd61b717c34ac
qubiak <kubiak1986@gmail.com>
2021-07-13 21:18:10 +0200

    init

---
+++ src/main/java/pl/qubiak/post/office/PostOfficeApplication.java
@@ -0,0 +1,13 @@
+package pl.qubiak.post.office;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostOfficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/qubiak/post/office/PostOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.qubiak.post.office;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
